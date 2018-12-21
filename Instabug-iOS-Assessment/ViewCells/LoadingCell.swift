b982d1de11a92073c398980f1aab67519026c9e5
Mahdy <ahmed.mahdy@vodafone.com>
2018-12-21 23:38:59 +0200

    Adding view cells + storyboard

---
+++ Instabug-iOS-Assessment/ViewCells/LoadingCell.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingCell.swift
+//  Instabug-iOS-Assessment
+//
+//  Created by ahmed mahdy on 12/21/18.
+//  Copyright © 2018 ahmed mahdy. All rights reserved.
+//
+
+import UIKit
+
+class LoadingCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+}
