bb5430d1a9058c04bbc2c1950f39c5780905ce5a
Mahdy <ahmed.mahdy@vodafone.com>
2018-12-21 12:45:42 +0200

    initial commit

---
+++ Instabug-iOS-Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instabug-iOS-Assessment
+//
+//  Created by ahmed mahdy on 12/21/18.
+//  Copyright © 2018 ahmed mahdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
